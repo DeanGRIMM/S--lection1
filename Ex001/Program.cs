1eb18b167da4d8f9f16058b1918a4ca3ff164ce7
Dmitry <dean.grimm@yandex.ru>
2023-01-06 03:12:38 +0800

    ex001

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное 
+// на само себя).
+// Например:
+// 4 -> 16 
+// -3 -> 9 
+// -7 -> 49
+
+
+// Задача №1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+//  Console.WriteLine(" Введите число 1  ");
+
+//  int x = Convert.ToInt32(Console.ReadLine());
+
+//  Console.WriteLine(" Введите число 2  ");
+
+//  int y = Convert.ToInt32(Console.ReadLine());
+
+//  if (x == y * y)
+//  {
+//      Console.WriteLine(" Первое число является кватдратом второго ");
+
+//  }
+//  else
+//  {
+//      Console.WriteLine(" Первое число не является кватратом второго ");
+
+
+// Console.WriteLine (" Введите число от 1 до 7");
+// int x = Convert.ToInt32 (Console.ReadLine());
+// if (x == 1) Console.WriteLine ("Понедельник");
+// else if (x == 2) Console.WriteLine ("Вторник");
+// else if (x == 3) Console.WriteLine ("Среда");
+// else if (x == 4) Console.WriteLine ("Четверг");
+// else if (x == 5) Console.WriteLine ("Пятница");
+// else if (x == 6) Console.WriteLine ("Суббота");
+// else if (x == 7) Console.WriteLine ("Воскр");
+// else Console.WriteLine (" Некорректное число");
+
+//вывести все чисда от 1 до введенного пользователем
+// Console.WriteLine("Введите целое число"); // приглашаем пользователя ввести информацию
+// int x = Convert.ToInt32( Console.ReadLine()); // перевели данные от пользователя в целое число и спрятали в переменную x тоже целочисленного типа
+// int i =1;
+// while (i<=x) 
+// {
+//     Console.Write(i + " ");
+//     i=i+1;
+// }
